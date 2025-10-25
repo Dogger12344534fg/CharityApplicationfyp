b96fd420da143721e9be24371fdf7613c8496249
Dipendra <dipendra10@192.168.101.3>
2025-10-25 20:46:48 +0545

    This is the start of the project

---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function Header(){
+return(
+  <header>
+    <h1 className='deep'>My website</h1>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>  
+      <li>Blog</li>
+      <li>Careers</li>
+    </nav>
+    <hr />
+  </header>
+);
+}
+
+export default Header;
