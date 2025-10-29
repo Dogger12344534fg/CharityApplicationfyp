4d3bb35d5704052097b53abf7e5f0a27cd741ee1
Dipendra <dipendra10@192.168.101.10>
2025-10-30 00:25:19 +0545

    THis is new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,7 @@
-import List from "./list";  
-function App() {
-    const fruits =[{ id : 1, name :"gr", cal :"h"},
-    {id : 2 ,name :"red", cal :"g"},
-    {id :3 ,name :"f", cal :"to"},
-    {id : 4,name :"deep", cal :"yo"}
-  ]
-
-    const veg =[{ id : 5, name :"celery", cal :"h"},
-    {id : 6 ,name :"carrot", cal :"g"},
-    {id :7 ,name :"cabbage", cal :"to"},
-    {id : 8,name :"corn", cal :"yo"}
-  ]
- 
+import Counter from './Counter.jsx';
+ function App() {
 return (<>
-  {fruits.length >0 &&<List items ={fruits} category ="Fruits"/>}
-   {veg.length > 0 &&  <List items ={veg} category ="veggies"/>}
+<Counter/>
   </>
 );
 }
--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,7 +1,21 @@
-import style from './Button.module.css'
+
 function Button(){
-  return(
-    <button className={style.button}>SignUp</button>
+
+
+    // let count = 0;
+    // const dep = (name)=> {
+    //   if (count < 0){
+    //     count++;
+    //     console.log(`${name} you clicked me ${count}`);
+    //   }else{
+    //     console.log(`${name} stop clicking me ${count}`);
+    //   }
+    // };
+
+    const eventhand = (e)=> e.target.textContent ="ouch";
+
+    return(
+    <button onDoubleClick={(e)=>eventhand(e)}>CLICK ME</button>
   );
 }
 export default Button
