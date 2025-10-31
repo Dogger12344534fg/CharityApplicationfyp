bee086b34f19845ac7450dff6018449a665569d4
Dipendra <dipendra10@Dipendras-MacBook-Pro.local>
2025-10-31 10:00:26 +0545

    this is third phase

--- src/renderlist/List.jsx
+++ src/renderlist/List.jsx
@@ -28,3 +28,22 @@ List.defaultProps={
 }
 
 export default List
+
+
+
+//add this to app.jsx
+
+//  const fruits =[{ id : 1, name :"gr", cal :"h"},
+//     {id : 2 ,name :"red", cal :"g"},
+//     {id :3 ,name :"f", cal :"to"},
+//     {id : 4,name :"deep", cal :"yo"}
+//   ]
+
+//     const veg =[{ id : 5, name :"celery", cal :"h"},
+//     {id : 6 ,name :"carrot", cal :"g"},
+//     {id :7 ,name :"cabbage", cal :"to"},
+//     {id : 8,name :"corn", cal :"yo"}
+//   ]
+
+  // {fruits.length >0 &&<List items ={fruits} category ="Fruits"/>}
+  //  {veg.length > 0 &&  <List items ={veg} category ="veggies"/>}
