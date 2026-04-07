36020fe5e6f65e9181a88028402347e3be8c1cac
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2026-04-07 21:48:05 +0545

    axios to handle campaign

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/auth.route");
+
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+
+// Health check
+app.get("/", (req, res) => {
+  res.json({ message: "API is running" });
+});
+
+// Start Server After Connecting to MongoDB Atlas
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+});
