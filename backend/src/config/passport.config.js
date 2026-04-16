9c9575e71bf40e457d4d4791e19adf381868f149
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2026-04-16 22:33:24 +0545

    this is passport.config.js

--- backend/src/config/passport.config.js
+++ backend/src/config/passport.config.js
@@ -9,7 +9,7 @@ passport.use(
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
         process.env.GOOGLE_CALLBACK_URL ||
-        "http://localhost:5000/api/auth/google/callback",
+        "http://localhost:1000/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
