dcf201d2cdecdd3ec021d36d26f3dfdd5100f956
DipendraRoka-js <np03cs4a230497@heraldcollege.edu.np>
2026-05-30 17:24:48 +0545

    fix: correct currency format for zero value in formatCurrency utility

--- frontend/src/utils/formatCurrency.ts
+++ frontend/src/utils/formatCurrency.ts
@@ -1,5 +1,5 @@
 export default function formatCurrency(value: number) {
-		if (!value) return "Rs";
+		if (!value) return "Rs.0";
 
 		if (value >= 1_000_000) {
 			return `Rs.${(value / 1_000_000).toFixed(1)}M`;
